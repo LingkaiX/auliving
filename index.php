ef81d3881fd01dd5f4c66340ce5f4182ea1d0c40
Lingkai.Xu <xlk910728@gmail.com>
2018-08-16 10:43:33 +1000

    Try sass framework

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php get_header(); ?>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.css" rel="stylesheet">
-    <style>
-        .pic {
-            max-width: 200px;
-        }
-    </style>
-        <header style="height: 48px; background-color: lightblue;"></header>
-    <main style="width: 1140px; height: 900px; background-color: white; margin: 0 auto;">
-        <img src="https://xenforo.com/community/media/kingfish.2125/full" alt="" height="200" width="300" class="pic">
+    <header style="height: 48px; background-color: yellow;"></header>
+    <main style="background-color: white; margin: 0 auto; display: flex;" class="container">
+        <div class="left">
+            <div class="b1" style="margin-bottom:10px;">【悉尼活动】悉尼婚礼展 时尚集市 悉尼茶节 悉尼皇家植物园花卉展</div>
+            <div class="b1" style="margin-bottom:10px;">【悉尼活动】悉尼婚礼展 时尚集市 悉尼茶节 悉尼皇家植物园花卉展1</div>
+            <div class="b1" style="height:120px;">【悉尼活动】悉尼婚礼展 时尚集市 悉尼茶节 悉尼皇家植物园花卉展</div>
+        </div>
+        <div class="center">
+            <img src="https://xenforo.com/community/media/kingfish.2125/full" alt="" width="640" height="360" class="pic">
+        </div>
+        <div class="right">
+            <div class="b2" style="margin-bottom:10px;">【悉尼活动】悉尼婚礼展 时尚集市 悉尼茶节 悉尼皇家植物园花卉展</div>
+            <div class="b2">【悉尼活动】悉尼婚礼展 时尚集市 悉尼茶节 悉尼皇家植物园花卉展</div>
+        </div>
     </main>
 <?php get_footer(); ?>
