cd3a481bf57e0852afd9ad864bf2d4f2f86e3697
Lingkai.Xu <xlk910728@gmail.com>
2018-08-21 11:44:27 +1000

    Add Header

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php get_header(); ?>
-    <header style="height: 48px; background-color: yellow;"></header>
-    <main style="background-color: white; margin: 0 auto; display: flex;" class="container">
+    <main class="container" style="display:none;">
         <div class="left">
             <div class="b1" style="margin-bottom:10px;">【悉尼活动】悉尼婚礼展 时尚集市 悉尼茶节 悉尼皇家植物园花卉展</div>
             <div class="b1" style="margin-bottom:10px;">【悉尼活动】悉尼婚礼展 时尚集市 悉尼茶节 悉尼皇家植物园花卉展1</div>
@@ -14,4 +13,23 @@
             <div class="b2">【悉尼活动】悉尼婚礼展 时尚集市 悉尼茶节 悉尼皇家植物园花卉展</div>
         </div>
     </main>
+
+    <?php 
+
+$count=0;
+    while (have_posts()) : the_post();
+    $count++;
+    if($count==25){
+        $query_top = new WP_Query( array( 'category_name' => 'top','posts_per_page' => 8 )  );
+
+        while ( $query_top->have_posts() ) {
+            $query_top->the_post();
+            echo '<li>' . get_the_title() . '</li>';
+        }
+        wp_reset_postdata();
+    }
+            the_title('<p>', '</p>');
+            endwhile;
+    ?>
 <?php get_footer(); ?>
+
