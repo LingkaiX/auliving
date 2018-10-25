606cc4251bed2b2449973d23cb2a76696972493f
Lingkai.Xu <xlk910728@gmail.com>
2018-10-25 16:27:10 +1100

    //

--- footer.php
+++ footer.php
@@ -16,8 +16,5 @@
 
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.17/vue.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script> -->
-<script>
-
-<script>
 </body>
 </html>
