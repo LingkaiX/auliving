f1b58acbf11b676122105ccb120e581b46978300
Lingkai.Xu <xlk910728@gmail.com>
2018-11-21 17:35:00 +1100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 
 <?php get_header(); ?>
-<main id="1">    
+<main>    
 <?php 
     $stickyPosts = get_field( 'sticky_posts', 'category_'.get_category_by_slug( 'top' )->term_id );
     $headSectionQuery = new WP_Query( array( 'category_name' => 'top','posts_per_page' => 8 )  );
-    print_r($stickyPosts);
-    print_r($headSectionQuery);
     include 'snippet/head-section.php';
     //print_r(get_taxonomy( 'top' ));
 ?>
