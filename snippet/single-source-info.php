b6831fc56f95cb847486c660bde1c090eaba274f
tvadsad <38453107+tvadsad@users.noreply.github.com>
2019-01-18 14:25:45 +1100

    Update single-source-info.php

--- snippet/single-source-info.php
+++ snippet/single-source-info.php
@@ -8,23 +8,6 @@ if($sourceInfo){
             <a href="<?php echo getBaseUrl(); ?>">澳洲生活网</a>
             并包含原文标题及链接：《<a href="<?php echo get_the_permalink(); ?>"><?php echo $post->post_title; ?></a>》
         </p>
-<<<<<<< HEAD
-        <?php if(get_field('article_sources')!=null){ ?>
-            <p>文章来源：<?php echo get_field('article_sources'); ?></p>
-        <?php}?>
-    <?php }if($sourceInfo['reproduced_article']==1){?>
-        <p>本文由 <a href="<?php echo getBaseUrl(); ?>">澳洲生活网</a> 转载发布，仅代表原作者或原平台观点，不代表本网站立场。
-            <a href="<?php echo getBaseUrl(); ?>">澳洲生活网</a>
-            仅提供信息发布平台，文章或有适当删改。
-        </p>
-        <p>文章来源：<?php echo get_field('reproduced_article_sources'); ?></p>
-    <?php }if($sourceInfo['contribution_article']==1){?>
-        <p>本文由<?php echo echo get_field('contributor'); ?>提供，仅代表原作者观点，不代表本网站立场。
-            <a href="<?php echo getBaseUrl(); ?>">澳洲生活网</a>
-            仅提供信息发布平台，文章或有适当删改。
-        </p>
-    <?php }if(is_array($sourceInfo['img_sources'])&&sizeof($sourceInfo['img_sources'])){
-=======
     <?php }else{
         if(is_array($sourceInfo['article_sources'])&&sizeof($sourceInfo['article_sources'])){
             echo '<p>文章来源：';
@@ -34,7 +17,6 @@ if($sourceInfo){
             echo '</p>';
         }
         if(is_array($sourceInfo['img_sources'])&&sizeof($sourceInfo['img_sources'])){
->>>>>>> parent of d46e157... Update single-source-info.php
             echo '<p>图片来源：';
             foreach($sourceInfo['img_sources'] as $s){
                 echo '<a class="img-source" href="'.$s['url'].'">'.$s['title'].'</a>';
