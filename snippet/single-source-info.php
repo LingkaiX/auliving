c5f03af2d8513480cd6fe1a10cc3eae0f4a5eeb9
tvadsad <38453107+tvadsad@users.noreply.github.com>
2019-01-18 15:37:37 +1100

    ADD two other sourse info policy

--- snippet/single-source-info.php
+++ snippet/single-source-info.php
@@ -8,14 +8,25 @@ if($sourceInfo){
             <a href="<?php echo getBaseUrl(); ?>">澳洲生活网</a>
             并包含原文标题及链接：《<a href="<?php echo get_the_permalink(); ?>"><?php echo $post->post_title; ?></a>》
         </p>
-    <?php }else{
-        if(is_array($sourceInfo['article_sources'])&&sizeof($sourceInfo['article_sources'])){
+        <?php if(is_array($sourceInfo['article_sources'])&&sizeof($sourceInfo['article_sources'])){
             echo '<p>文章来源：';
             foreach($sourceInfo['article_sources'] as $s){
                 echo '<a class="article-source" href="'.$s['url'].'">'.$s['title'].'</a>';
             }
             echo '</p>';
-        }
+        }?>
+    <?php }if($sourceInfo['reproduced_article']==1){?>
+        <p>本文由 <a href="<?php echo getBaseUrl(); ?>">澳洲生活网</a> 转载发布，仅代表原作者或原平台观点，不代表本网站立场。
+            <a href="<?php echo getBaseUrl(); ?>">澳洲生活网</a>
+            仅提供信息发布平台，文章或有适当删改。
+        </p>
+        <p>文章来源：<?php echo get_field('reproduced_article_sources'); ?></p>
+    <?php }if($sourceInfo['contribution_article']==1){?>
+        <p>本文由<?php echo get_field('contributor'); ?>提供，仅代表原作者观点，不代表本网站立场。
+            <a href="<?php echo getBaseUrl(); ?>">澳洲生活网</a>
+            仅提供信息发布平台，文章或有适当删改。
+        </p>
+    <?php }if($sourceInfo['has_img_sources']==1){
         if(is_array($sourceInfo['img_sources'])&&sizeof($sourceInfo['img_sources'])){
             echo '<p>图片来源：';
             foreach($sourceInfo['img_sources'] as $s){
