8f37ee1b54db12d9aa5404ef490b0aee01d22b0e
tvadsad <38453107+tvadsad@users.noreply.github.com>
2019-01-18 14:24:35 +1100

    Update single-source-info.php

--- snippet/single-source-info.php
+++ snippet/single-source-info.php
@@ -8,6 +8,7 @@ if($sourceInfo){
             <a href="<?php echo getBaseUrl(); ?>">澳洲生活网</a>
             并包含原文标题及链接：《<a href="<?php echo get_the_permalink(); ?>"><?php echo $post->post_title; ?></a>》
         </p>
+<<<<<<< HEAD
         <?php if(get_field('article_sources')!=null){ ?>
             <p>文章来源：<?php echo get_field('article_sources'); ?></p>
         <?php}?>
@@ -23,6 +24,17 @@ if($sourceInfo){
             仅提供信息发布平台，文章或有适当删改。
         </p>
     <?php }if(is_array($sourceInfo['img_sources'])&&sizeof($sourceInfo['img_sources'])){
+=======
+    <?php }else{
+        if(is_array($sourceInfo['article_sources'])&&sizeof($sourceInfo['article_sources'])){
+            echo '<p>文章来源：';
+            foreach($sourceInfo['article_sources'] as $s){
+                echo '<a class="article-source" href="'.$s['url'].'">'.$s['title'].'</a>';
+            }
+            echo '</p>';
+        }
+        if(is_array($sourceInfo['img_sources'])&&sizeof($sourceInfo['img_sources'])){
+>>>>>>> parent of d46e157... Update single-source-info.php
             echo '<p>图片来源：';
             foreach($sourceInfo['img_sources'] as $s){
                 echo '<a class="img-source" href="'.$s['url'].'">'.$s['title'].'</a>';
