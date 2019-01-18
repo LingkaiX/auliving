68c7d86245af5d0f4ed6ca367ecf69a902c4f527
tvadsad <38453107+tvadsad@users.noreply.github.com>
2019-01-18 15:16:49 +1100

    Update single-source-info.php

--- snippet/single-source-info.php
+++ snippet/single-source-info.php
@@ -8,12 +8,14 @@ if($sourceInfo){
             <a href="<?php echo getBaseUrl(); ?>">澳洲生活网</a>
             并包含原文标题及链接：《<a href="<?php echo get_the_permalink(); ?>"><?php echo $post->post_title; ?></a>》
         </p>
-        <?php if(is_array($sourceInfo['article_sources'])&&sizeof($sourceInfo['article_sources'])){
+    <?php }else{
+        if(is_array($sourceInfo['article_sources'])&&sizeof($sourceInfo['article_sources'])){
             echo '<p>文章来源：';
             foreach($sourceInfo['article_sources'] as $s){
                 echo '<a class="article-source" href="'.$s['url'].'">'.$s['title'].'</a>';
             }
             echo '</p>';
+<<<<<<< HEAD
         }?>
     <?php }if($sourceInfo['reproduced_article']==1){?>
         <p>本文由 <a href="<?php echo getBaseUrl(); ?>">澳洲生活网</a> 转载发布，仅代表原作者或原平台观点，不代表本网站立场。
@@ -27,6 +29,9 @@ if($sourceInfo){
             仅提供信息发布平台，文章或有适当删改。
         </p>
     <?php }if($sourceInfo['has_img_sources']==1){
+=======
+        }
+>>>>>>> parent of 2a221ac... add two other sourse info policy
         if(is_array($sourceInfo['img_sources'])&&sizeof($sourceInfo['img_sources'])){
             echo '<p>图片来源：';
             foreach($sourceInfo['img_sources'] as $s){
