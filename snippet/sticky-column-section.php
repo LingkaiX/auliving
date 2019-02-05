6184a0abb2e88b87749733906b8c254d951121ba
tvadsad <38453107+tvadsad@users.noreply.github.com>
2019-02-05 12:28:26 +1100

    加专栏标题

--- snippet/sticky-column-section.php
+++ snippet/sticky-column-section.php
@@ -19,6 +19,7 @@ if(countSafely($stickyColumns)>=3){
 </style>
 <section class="sticky-columns">
 <div class="container">
+    <span>专栏</span>
     <div id="sticky-columns" class="owl-carousel owl-theme">
         <?php
             foreach($stickyColumns as $columnId){
