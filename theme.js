0ef6786383fbb31ce3d44dd600163e97d0bd10c2
tvadsad <38453107+tvadsad@users.noreply.github.com>
2019-02-06 11:45:22 +1100

    watch

--- theme.js
+++ theme.js
@@ -1,6 +1,446 @@
-parcelRequire=function(e,r,n,t){var i="function"==typeof parcelRequire&&parcelRequire,o="function"==typeof require&&require;function u(n,t){if(!r[n]){if(!e[n]){var f="function"==typeof parcelRequire&&parcelRequire;if(!t&&f)return f(n,!0);if(i)return i(n,!0);if(o&&"string"==typeof n)return o(n);var c=new Error("Cannot find module '"+n+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[n][1][r]||r},p.cache={};var l=r[n]=new u.Module(n);e[n][0].call(l.exports,p,l,l.exports,this)}return r[n].exports;function p(e){return u(p.resolve(e))}}u.isParcelRequire=!0,u.Module=function(e){this.id=e,this.bundle=u,this.exports={}},u.modules=e,u.cache=r,u.parent=i,u.register=function(r,n){e[r]=[function(e,r){r.exports=n},{}]};for(var f=0;f<n.length;f++)u(n[f]);if(n.length){var c=u(n[n.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=c:"function"==typeof define&&define.amd?define(function(){return c}):t&&(this[t]=c)}return u}({"1ed9":[function(require,module,exports) {
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
 
-},{}],"m3VC":[function(require,module,exports) {
-"use strict";function e(){var e=window.navigator.userAgent,r=e.indexOf("MSIE ");if(r>0)return parseInt(e.substring(r+5,e.indexOf(".",r)),10);if(e.indexOf("Trident/")>0){var a=e.indexOf("rv:");return parseInt(e.substring(a+3,e.indexOf(".",a)),10)}return!1}require("./theme.scss"),window.loadMoreArticles=function(e,r,a,t,s,n){var i=parseInt(jQuery(e).data("offset")),o=jQuery(e).data("loading"),d=jQuery(e).data("nomore");0==o&&(o=!0,jQuery(e).data("loading",o),jQuery(e+" span").text(s).removeClass("loaded").addClass("loading"),jQuery.getJSON(t+"&offset="+i,function(t){if(t.length>0){var s=[];jQuery.each(t,function(e,r){var a="",t=!1;jQuery.each(r.categories,function(e,r){"choiceofeditor"==r.slug&&(t=!0)}),a=t?'<div class="listed-post-special">':'<div class="listed-post">',a+='<a class="cover-img" target="_blank" href="'+r.link+'" style="background-image:url(\''+r.cover_img+'\');"><div class="post-info"><a target="_blank" href="'+r.link+'"><h4 class="title">'+r.title.rendered+'</h4></a><h6 class="sub-title">'+r.excerpt.rendered+'</h6><span class="date-info">'+r.date_info+"</span></div></div>",s.push(a)}),jQuery(r).append(s),i+=a,jQuery(e).data("offset",i),jQuery(e+" span").text(n).addClass("loaded").removeClass("loading")}t.length<a&&(jQuery(e+"-outer").hide(),jQuery(e).attr("disabled",!0),d=!0)}).fail(function(){jQuery(e+" span").text(n).addClass("loaded").removeClass("loading")}).always(function(){o=!1,jQuery(e).data("loading",o),jQuery(e).data("nomore",d)}))},jQuery(document).ready(function(e){jQuery("img").on("dragstart",function(e){e.preventDefault()});var r=parseInt(jQuery("#wpadminbar").css("height"));if(r){var a=jQuery(".body-header");a&&a.css("top",r)}}),e()&&alert("Sorry, in order to provide a better browsing experience, our website no longer supports IE browsers. Please consider a modern browser, such as Chrome or Firefox. Thank you!");
-},{"./theme.scss":"1ed9"}]},{},["m3VC"], null)
+// eslint-disable-next-line no-global-assign
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({"../../../../../../../Users/Desktop2/AppData/Roaming/npm/node_modules/parcel/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
+
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+
+  return bundleURL;
+}
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp):\/\/.+)\/[^/]+$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"../../../../../../../Users/Desktop2/AppData/Roaming/npm/node_modules/parcel/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"../../../../../../../Users/Desktop2/AppData/Roaming/npm/node_modules/parcel/src/builtins/bundle-url.js"}],"theme.scss":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"_css_loader":"../../../../../../../Users/Desktop2/AppData/Roaming/npm/node_modules/parcel/src/builtins/css-loader.js"}],"theme.js":[function(require,module,exports) {
+"use strict";
+
+require("./theme.scss");
+
+window.loadMoreArticles = function (selector, pushTo, perPage, url, loadingString, moreString) {
+  var postOffset = parseInt(jQuery(selector).data("offset"));
+  var postLoading = jQuery(selector).data("loading");
+  var noMorePost = jQuery(selector).data("nomore");
+
+  if (postLoading == false) {
+    postLoading = true;
+    jQuery(selector).data("loading", postLoading);
+    jQuery(selector + " span").text(loadingString).removeClass("loaded").addClass("loading");
+    jQuery.getJSON(url + "&offset=" + postOffset, function (data) {
+      if (data.length > 0) {
+        var posts = [];
+        jQuery.each(data, function (key, val) {
+          var s = "";
+          var isEditorsChoice = false;
+          jQuery.each(val.categories, function (key, cate) {
+            if (cate.slug == "choiceofeditor") isEditorsChoice = true;
+          });
+          s = isEditorsChoice ? '<div class="listed-post-special">' : '<div class="listed-post">';
+          s += '<a class="cover-img" target="_blank" href="' + val.link + '" style="background-image:url(' + "'" + val.cover_img + "'" + ');"><div class="post-info"><a target="_blank" href="' + val.link + '"><h4 class="title">' + val.title.rendered + '</h4></a><h6 class="sub-title">' + val.excerpt.rendered + '</h6><span class="date-info">' + val.date_info + "</span></div></div>";
+          posts.push(s);
+        });
+        jQuery(pushTo).append(posts);
+        postOffset += perPage;
+        jQuery(selector).data("offset", postOffset);
+        jQuery(selector + " span").text(moreString).addClass("loaded").removeClass("loading");
+      }
+
+      if (data.length < perPage) {
+        jQuery(selector + "-outer").hide();
+        jQuery(selector).attr("disabled", true);
+        noMorePost = true;
+      }
+    }).fail(function () {
+      jQuery(selector + " span").text(moreString).addClass("loaded").removeClass("loading");
+    }).always(function () {
+      postLoading = false;
+      jQuery(selector).data("loading", postLoading);
+      jQuery(selector).data("nomore", noMorePost);
+    });
+  }
+};
+
+jQuery(document).ready(function ($) {
+  jQuery("img").on("dragstart", function (event) {
+    event.preventDefault();
+  });
+  var h = parseInt(jQuery("#wpadminbar").css("height"));
+
+  if (h) {
+    var header = jQuery(".body-header");
+
+    if (header) {
+      header.css("top", h);
+    }
+  }
+});
+/**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ * refered from: https://stackoverflow.com/a/21712356
+ */
+
+function detectIE() {
+  var ua = window.navigator.userAgent;
+  var msie = ua.indexOf("MSIE ");
+
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)), 10);
+  }
+
+  var trident = ua.indexOf("Trident/");
+
+  if (trident > 0) {
+    // IE 11 => return version number
+    var rv = ua.indexOf("rv:");
+    return parseInt(ua.substring(rv + 3, ua.indexOf(".", rv)), 10);
+  } // var edge = ua.indexOf("Edge/");
+  // if (edge > 0) {
+  //   // Edge (IE 12+) => return version number
+  //   return parseInt(ua.substring(edge + 5, ua.indexOf(".", edge)), 10);
+  // }
+  // other browser
+
+
+  return false;
+}
+
+if (detectIE()) {
+  alert("Sorry, in order to provide a better browsing experience, our website no longer supports IE browsers. Please consider a modern browser, such as Chrome or Firefox. Thank you!");
+}
+},{"./theme.scss":"theme.scss"}],"../../../../../../../Users/Desktop2/AppData/Roaming/npm/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+var parent = module.bundle.parent;
+
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61454" + '/');
+
+  ws.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      console.clear();
+      data.assets.forEach(function (asset) {
+        hmrApply(global.parcelRequire, asset);
+      });
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          hmrAccept(global.parcelRequire, asset.id);
+        }
+      });
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID; // html encode message and stack trace
+
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAccept(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAccept(bundle.parent, id);
+  }
+
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAccept(global.parcelRequire, id);
+  });
+}
+},{}]},{},["../../../../../../../Users/Desktop2/AppData/Roaming/npm/node_modules/parcel/src/builtins/hmr-runtime.js","theme.js"], null)
 //# sourceMappingURL=/theme.map
