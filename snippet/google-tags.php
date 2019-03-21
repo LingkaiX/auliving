effd60575f9d5af9f11542064820318e6a91988b
tvadsad <38453107+tvadsad@users.noreply.github.com>
2019-03-21 15:59:23 +1100

    Update google-tags.php

--- snippet/google-tags.php
+++ snippet/google-tags.php
@@ -7,3 +7,14 @@
 
   gtag('config', 'UA-60850841-1');
 </script>
+
+
+<!-- Global site tag (gtag.js) - Google Ads: 782938121 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-782938121"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-782938121');
+</script>
