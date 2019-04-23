00fbe5acbbf8a4da40d5735d85d3fdd010d4717a
tvadsad <38453107+tvadsad@users.noreply.github.com>
2019-04-23 16:14:15 +1000

    专栏页面

--- page-specialcolumn.php
+++ page-specialcolumn.php
@@ -1,11 +1,65 @@
 <?php get_header(); ?>
+<?php
+/*
+Need data: 
+    Array<Category_ID>: $stickyColumns
+*/
+//print_r($stickyColumns);
+    $stickyColumns = get_field( 'sticky_columns', 'category_'.get_category_by_slug( 'top' )->term_id );
+    $stickyColumnsCate = get_field( 'sticky_columns', 'category_'.get_category_by_slug( 'classification' )->term_id );
+    //print_r($stickyColumnsCate);
+?>
 <main>
-    <section class="first-section container">
-    <?php
-        while ( have_posts() ) : the_post();
-        the_content();
-        endwhile;
-    ?>  
+<?php include 'snippet/side-ads.php';?>
+<aside class="gam-aulv aulv-h1">
+    <!-- /21666183985/aulv/aulv-h1 -->
+    <div id='div-gpt-ad-1543362254773-0'>
+    <script>
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1543362254773-0'); });
+    </script>
+    </div>
+</aside>
+    <section class="special-column">
+        <div class="container">
+            <div class="classi">
+            <span id="justASpan">专栏分类</span>
+                <?php foreach($stickyColumnsCate as $cateId){?> 
+                    <a  onclick="classi_display(<?php echo $cateId; ?>)"><?php echo get_cat_name($cateId); ?></a>
+                <?php } ?>
+            </div>
+            <div class="sticky-columns">
+                <?php
+                    foreach($stickyColumns as $columnId){
+                        $fields=get_fields('category_'.$columnId );
+                        if(!isset($fields['cover_img']['sizes']['thumbnail'])) continue;
+                        //if(!isset($fields['thumbnail_img']['sizes']['thumbnail'])) continue;
+                        //$scQuery=new WP_Query( array( 'cat' => $columnId, 'posts_per_page' => 3, 'ignore_sticky_posts' => 1));
+                        //if(count($scQuery->posts)!=3) continue;
+                ?>
+                    <div  class="column <?php echo $fields['sticky_columns_cate']; ?>"><a href="<?php echo get_category_link($columnId); ?>">
+                        <div class="thumbnail_img" style="background-image:url('<?php echo $fields['thumbnail_img']['sizes']['thumbnail']?>');"></div>
+                        <div class="cover-img" style="background-image:url('<?php echo $fields['cover_img']['sizes']['thumbnail']?>');"></div>
+                        <h4 class="title"><a href="<?php echo get_category_link($columnId); ?>"><?php echo get_cat_name($columnId); ?></a></h4>
+                        
+                        <div class="desc"><?php echo category_description($columnId); ?></div>
+                         
+                    </a></div>
+                <?php } ?>
+            </div>
+        </div>
     </section>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+        function classi_display(id){
+            $('.column').slideUp(1);
+            $('.'+id).slideDown();
+        }
+        $(document).ready(function(){
+            $("#justASpan").click(function(){
+                $('.column').slideDown();
+            });
+        });
+    </script>
 </main>
+
 <?php get_footer(); ?>
