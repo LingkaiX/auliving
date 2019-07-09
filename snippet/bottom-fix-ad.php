91a6df2db3d9e581b4bb42bec5778ced90725ee2
tvadsad <38453107+tvadsad@users.noreply.github.com>
2019-07-09 12:25:49 +1000

    bfa

---
+++ snippet/bottom-fix-ad.php
@@ -0,0 +1,12 @@
+<section class="bfa">
+    <aside id="bfa" class="gam-aulv aulv-bfa">
+        <!-- /21666183985/aulv/aulv-bottom-fixed -->
+        <div id='div-gpt-ad-1562633073617-0'>
+            <span onclick="document.getElementById('bfa').style.display='none'" 
+                class="ad-close-button">&times;</span>
+            <script>
+                googletag.cmd.push(function() { googletag.display('div-gpt-ad-1562633073617-0'); });
+            </script>
+        </div>
+    </aside>
+</section>
