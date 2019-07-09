38d445bc6deb99c029f8c7bf737830646d981684
tvadsad <38453107+tvadsad@users.noreply.github.com>
2019-07-09 12:29:22 +1000

    Update bottom-fix-ad.php

--- snippet/bottom-fix-ad.php
+++ snippet/bottom-fix-ad.php
@@ -2,11 +2,11 @@
     <aside id="bfa" class="gam-aulv aulv-bfa">
         <!-- /21666183985/aulv/aulv-bottom-fixed -->
         <div id='div-gpt-ad-1562633073617-0'>
-            <span onclick="document.getElementById('bfa').style.display='none'" 
-                class="ad-close-button">&times;</span>
             <script>
                 googletag.cmd.push(function() { googletag.display('div-gpt-ad-1562633073617-0'); });
             </script>
         </div>
+        <span onclick="document.getElementById('bfa').style.display='none'" 
+                class="ad-close-button">&times;</span>
     </aside>
 </section>
