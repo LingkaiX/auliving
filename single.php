0589bbcd13573dcc3d70718c3822e9a7c887edb0
tvadsad <38453107+tvadsad@users.noreply.github.com>
2020-03-06 10:01:17 +1100

    Update single.php

--- single.php
+++ single.php
@@ -3,7 +3,7 @@
     <!-- /21666183985/aulv/aulv-lb -->
     <div id='div-gpt-ad-1543362198289-0'>
     <script>
-    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1543362198289-0'); });
+        googletag.cmd.push(function() { googletag.display('div-gpt-ad-1543362198289-0'); });
     </script>
     </div>
 </aside>
@@ -57,7 +57,7 @@
                     </div>
                 </div>
             </section>
-            <aside class="gam-aulv aulv-a1">
+            <aside class=" aulv-a1">
                 <!-- /21666183985/aulv/aulv-a1 -->
                 <div id='div-gpt-ad-1582517809274-0'>
                     <script>
