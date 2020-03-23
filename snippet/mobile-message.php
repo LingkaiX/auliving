c00da7841b7e3597523992000fba18177ef4cfff
tvadsad <38453107+tvadsad@users.noreply.github.com>
2020-03-23 16:39:02 +1100

    Jade要加的手机上显示的信息

---
+++ snippet/mobile-message.php
@@ -0,0 +1,7 @@
+<section id="mm" class="mobile-message">
+    <br>
+    <span>点击下方<img src="https://img.icons8.com/ios/20/000000/upload.png"/>按钮，再点击【添加到主屏幕】</span>
+    <p></p>
+        <span onclick="document.getElementById('mm').style.display='none'" 
+                class="close-button">&times;</span>
+</section>
