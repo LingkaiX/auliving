01c8be9e87422ee23d477a56f804743970008349
tvadsad <38453107+tvadsad@users.noreply.github.com>
2020-03-24 16:09:10 +1100

    Update mobile-message.php

--- snippet/mobile-message.php
+++ snippet/mobile-message.php
@@ -1,6 +1,6 @@
 <section id="mm" class="mobile-message">
     <br>
-    <span>点击下方<img src="https://img.icons8.com/ios/20/000000/upload.png"/>按钮，再点击【添加到主屏幕】</span>
+    <span>实时了解澳洲最新动态，点击下方<img src="https://img.icons8.com/ios/20/000000/upload.png"/>按钮，再点击【添加到主屏幕】</span>
     <p></p>
         <span onclick="document.getElementById('mm').style.display='none'" 
                 class="close-button">&times;</span>
